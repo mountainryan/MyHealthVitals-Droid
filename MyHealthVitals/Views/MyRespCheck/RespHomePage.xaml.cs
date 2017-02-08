019fa6dfe1d7e25275c4d5087b3b0ee82d304c18
Hari Krishna Bista <harikrista@gmail.com>
2017-02-07 23:20:26 -0600

    Home commits remove BLE plugin added some design for MyResp Home page

---
+++ MyHealthVitals/Views/MyRespCheck/RespHomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MyHealthVitals
+{
+	public partial class RespHomePage : ContentPage
+	{
+		public RespHomePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
